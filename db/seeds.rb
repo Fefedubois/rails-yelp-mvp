93209a44553ab50ba9c07033c93b94bd26e13009
Felix Dubois <felixdubois@hotmail.fr>
2021-11-10 15:18:10 +0100

    fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create name: "Ma fourchette", address: "allée du vallon", phone_number: "0634343434", category: "italian"
+Restaurant.create name: "Ma Cuillere", address: "allée du domaine", phone_number: "0634343434", category: "belgian"
+Restaurant.create name: "Les délices", address: "allée du cahteau", phone_number: "0634443434", category: "chinese"
+Restaurant.create name: "Au bon plat", address: "allée du vallonette", phone_number: "0634343434", category: "french"
+Restaurant.create name: "Pizza pize", address: " 4 allée du vallon", phone_number: "0634393434", category: "chinese"
+Restaurant.create name: "Mon bon repas", address: "RUE du vallon", phone_number: "0634343434", category: "belgian"
+Restaurant.create name: "Ma salade", address: "cours du vallon", phone_number: "0634343434", category: "italian"
+Restaurant.create name: "Bistrot regent", address: "chateau du vallon", phone_number: "0634343434", category: "french"
+Restaurant.create name: "Bistro Tintino", address: "allée du vallée", phone_number: "0634343434", category: "italian"
+Restaurant.create name: "Ma cantine bio", address: "allée du rateau", phone_number: "0634393434", category: "french"
